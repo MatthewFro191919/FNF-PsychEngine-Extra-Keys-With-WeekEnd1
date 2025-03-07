79dae166e61ba34b16804371bfb1537037e72c03
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-03-07 14:10:33 -0500

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -12,6 +12,7 @@ haxelib install lime 8.1.2
 haxelib install openfl 9.3.3
 haxelib install flixel 5.6.1
 haxelib install flixel-addons 3.2.2
+haxelib install flixel-ui 2.5.0
 haxelib install flixel-tools 1.5.1
 haxelib install hscript-iris 1.1.3
 haxelib install tjson 1.4.0
@@ -22,6 +23,7 @@ haxelib set openfl 9.3.3
 haxelib install hxCodec 2.5.1
 haxelib install tjson
 haxelib install json2object
+haxelib install hxcpp-debug-server 1.2.4 
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
